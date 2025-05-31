3558f9588beb8b81f3970e6152c9beb2be7f53fc
Pedro Vieira <160655744+PedroVSX@users.noreply.github.com>
2025-05-31 12:29:58 -0300

    feat: setando interface do projeto

--- src/main/java/baseball/BaseballElimination.java
+++ src/main/java/baseball/BaseballElimination.java
@@ -1,4 +1,43 @@
 package baseball;
 
+import edu.princeton.cs.algs4.*;
+import java.util.*;
+
 public class BaseballElimination {
+
+    public BaseballElimination(String filename) {
+
+    }
+
+    public int numberOfTeams() {
+        return -1;
+    }
+
+    public Iterable<String> teams() {
+        return null;
+    }
+
+    public int wins(String team) {
+        return -1;
+    }
+
+    public int losses(String team) {
+        return -1;
+    }
+
+    public int remaining(String team) {
+        return -1;
+    }
+
+    public int against(String team1, String team2) {
+        return -1;
+    }
+
+    public boolean isEliminated(String team) {
+        return false;
+    }
+
+    public Iterable<String> certificateOfElimination(String team) {
+        return null;
+    }
 }
