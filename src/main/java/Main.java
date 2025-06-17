fc9c4d42627c24dc14a783c365b1d4d7dbeae42c
AlvesAlbano <samir.saar@gmail.com>
2025-06-16 21:47:26 -0300

    feat: adicionei a main

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import baseball.BaseballElimination;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int opcao;
+        final String caminho = "src/main/java/baseball/teams/";
+
+        final String[] times = {
+                "teams1.txt",
+                "teams10.txt",
+                "teams12-allgames.txt",
+                "teams12.txt",
+                "teams24.txt",
+                "teams29.txt",
+                "teams30.txt",
+                "teams32.txt",
+                "teams36.txt",
+                "teams4.txt",
+                "teams42.txt",
+                "teams48.txt",
+                "teams4a.txt",
+                "teams4b.txt",
+                "teams5.txt",
+                "teams50.txt",
+                "teams54.txt",
+                "teams5a.txt",
+                "teams5b.txt",
+                "teams5c.txt",
+                "teams60.txt",
+                "teams7.txt",
+                "teams8.txt",
+        };
+
+
+        for(int i = 0;i < times.length;i++){
+            System.out.printf("%d - %s\n",i,times[i]);
+        }
+
+        System.out.println("Selecione um arquivo: ");
+        opcao = input.nextInt();
+//        opcao = 3;
+
+        String arquivoEscolhido = caminho + times[opcao];
+        BaseballElimination.main(new String[]{arquivoEscolhido});
+    }
+}
